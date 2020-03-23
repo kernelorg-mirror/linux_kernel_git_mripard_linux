1f4805b768ea0a53c27105bf5cbb954c41091a12
Maxime Ripard <maxime@cerno.tech>
2020-03-23 11:56:59 +0100

    fixup! clk: bcm: Add BCM2711 DVP driver

--- drivers/clk/bcm/clk-bcm2711-dvp.c
+++ drivers/clk/bcm/clk-bcm2711-dvp.c
@@ -14,13 +14,13 @@
 #define NR_RESETS	6
 
 struct clk_dvp {
-	struct clk			*clks[NR_CLOCKS];
-	struct clk_onecell_data		clk_data;
+	struct clk_hw_onecell_data	*data;
 	struct reset_simple_data	reset;
 };
 
 static int clk_dvp_probe(struct platform_device *pdev)
 {
+	struct clk_hw_onecell_data *data;
 	struct resource *res;
 	struct clk_dvp *dvp;
 	void __iomem *base;
@@ -31,6 +31,13 @@ static int clk_dvp_probe(struct platform_device *pdev)
 	if (!dvp)
 		return -ENOMEM;
 	platform_set_drvdata(pdev, dvp);
+
+	dvp->data = devm_kzalloc(&pdev->dev,
+				 struct_size(dvp->data, hws, NR_CLOCKS),
+				 GFP_KERNEL);
+	if (!dvp->data)
+		return -ENOMEM;
+	data = dvp->data;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	base = devm_ioremap_resource(&pdev->dev, res);
@@ -52,39 +59,38 @@ static int clk_dvp_probe(struct platform_device *pdev)
 	if (!parent)
 		goto unregister_reset;
 
-	dvp->clks[0] = clk_hw_register_gate(&pdev->dev, "hdmi0-108MHz",
+	data->hws[0] = clk_hw_register_gate(&pdev->dev, "hdmi0-108MHz",
 					    parent, 0,
 					    base + DVP_HT_RPI_MISC_CONFIG, 3,
 					    CLK_GATE_SET_TO_DISABLE, &dvp->reset.lock);
-	if (IS_ERR(dvp->clks[0])) {
-		ret = PTR_ERR(dvp->clks[0]);
+	if (IS_ERR(data->hws[0])) {
+		ret = PTR_ERR(data->hws[0]);
 		goto unregister_reset;
 	}
 
-	dvp->clks[1] = clk_hw_register_gate(&pdev->dev, "hdmi1-108MHz",
+	data->hws[1] = clk_hw_register_gate(&pdev->dev, "hdmi1-108MHz",
 					    parent, 0,
 					    base + DVP_HT_RPI_MISC_CONFIG, 4,
 					    CLK_GATE_SET_TO_DISABLE, &dvp->reset.lock);
-	if (IS_ERR(dvp->clks[1])) {
-		ret = PTR_ERR(dvp->clks[1]);
+	if (IS_ERR(data->hws[1])) {
+		ret = PTR_ERR(data->hws[1]);
 		goto unregister_clk0;
 	}
 
-	dvp->clk_data.clks = dvp->clks;
-	dvp->clk_data.clk_num = NR_CLOCKS;
+	data->num = NR_CLOCKS;
 	ret = of_clk_add_hw_provider(pdev->dev.of_node, of_clk_hw_onecell_get,
-				     &dvp->clk_data);
+				     data);
 	if (ret)
 		goto unregister_clk1;
 
 	return 0;
 
 
-unregister_clk0:
-	clk_hw_unregister_gate(dvp->clks[1]);
+unregister_clk1:
+	clk_hw_unregister_gate(data->hws[1]);
 
 unregister_clk0:
-	clk_hw_unregister_gate(dvp->clks[0]);
+	clk_hw_unregister_gate(data->hws[0]);
 
 unregister_reset:
 	reset_controller_unregister(&dvp->reset.rcdev);
@@ -94,9 +100,10 @@ unregister_reset:
 static int clk_dvp_remove(struct platform_device *pdev)
 {
 	struct clk_dvp *dvp = platform_get_drvdata(pdev);
+	struct clk_hw_onecell_data *data = dvp->data;
 
-	clk_hw_unregister_gate(dvp->clks[1]);
-	clk_hw_unregister_gate(dvp->clks[0]);
+	clk_hw_unregister_gate(data->hws[1]);
+	clk_hw_unregister_gate(data->hws[0]);
 	reset_controller_unregister(&dvp->reset.rcdev);
 
 	return 0;
