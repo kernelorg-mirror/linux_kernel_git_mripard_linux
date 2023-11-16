b3e7071af3fcb0fd82437c3e36254146168af2bc
Guan-Yu Lin <guanyulin@google.com>
2023-11-16 16:32:16 +0800

    usb: typec: tcpm: skip checking port->send_discover in PD3.0
    The original Collison Avoidance mechanism, port->send_discover, avoids
    the conflict when port partners start AMS almost the same time. However,
    this mechanism is replaced by SINK_TX_OK and SINK_TX_NG. Skip the check
    in PD3.0 to avoid the deadlock when source is requesting DR_SWAP where
    sink is requesting DISCOVER_IDENTITY.
    
    Signed-off-by: Guan-Yu Lin <guanyulin@google.com>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20231116083221.1201892-1-guanyulin@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2859,7 +2859,7 @@ static void tcpm_pd_ctrl_request(struct tcpm_port *port,
 					   PD_MSG_CTRL_NOT_SUPP,
 					   NONE_AMS);
 		} else {
-			if (port->send_discover) {
+			if (port->send_discover && port->negotiated_rev < PD_REV30) {
 				tcpm_queue_message(port, PD_MSG_CTRL_WAIT);
 				break;
 			}
@@ -2875,7 +2875,7 @@ static void tcpm_pd_ctrl_request(struct tcpm_port *port,
 					   PD_MSG_CTRL_NOT_SUPP,
 					   NONE_AMS);
 		} else {
-			if (port->send_discover) {
+			if (port->send_discover && port->negotiated_rev < PD_REV30) {
 				tcpm_queue_message(port, PD_MSG_CTRL_WAIT);
 				break;
 			}
@@ -2884,7 +2884,7 @@ static void tcpm_pd_ctrl_request(struct tcpm_port *port,
 		}
 		break;
 	case PD_CTRL_VCONN_SWAP:
-		if (port->send_discover) {
+		if (port->send_discover && port->negotiated_rev < PD_REV30) {
 			tcpm_queue_message(port, PD_MSG_CTRL_WAIT);
 			break;
 		}
